89a429ce5428d098f2f2ff352eee9bccb5cda661
Tên của bạn <Email ca bn>
2026-04-13 23:32:04 +0700

    fix pop up again

--- frontend/src/components/GoogleReviews.jsx
+++ frontend/src/components/GoogleReviews.jsx
@@ -411,12 +411,16 @@ export default function GoogleReviews() {
 
   useEffect(() => {
     if (selectedReview) {
+      const scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
       document.body.style.overflow = 'hidden';
+      document.body.style.paddingRight = `${scrollBarWidth}px`;
     } else {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = '';
+      document.body.style.paddingRight = '';
     }
     return () => {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = '';
+      document.body.style.paddingRight = '';
     };
   }, [selectedReview]);
 
